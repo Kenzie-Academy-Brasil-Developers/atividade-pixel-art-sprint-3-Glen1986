b245bd189bd55447807cf2b20e1589355fc5ee0b
glen1986 <escobar.glen@gmail.com>
2021-07-15 10:50:35 -0300

    color pixel

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let currentColor = '#000';
+const input = document.getElementById('colorPicker');
+const container = document.getElementsByClassName('container')[0];
 
+const createTable = () => {
+    for (let i = 0; i < 20; i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        for (let j = 0; j < 20; j++) {
+            const pixel = document.createElement('span');
+            // ADICIONA A CLASSE QUE POSSUI OS ESTILOES PADRÃO DOS QUADRADOS DA TABELA
+            pixel.classList.add('pixel');
+            pixel.addEventListener('click', () => updateColor(currentColor, pixel));
+            line.appendChild(pixel);
+        }
+        container.appendChild(line);
+    }
+}
+const changeCurrentColor = (e) => {
+    currentColor = e.target.value;
+    console.log(currentColor)
+}
+const updateColor = (currentColor, element) => {
+    //SELECIONE O ELEMENTO E ATUALIZE A PROPRIEDADE BACKGROUND CONFORME A VARIAVEL
+    //changeCurrentColor(currentColor) => element.style.backgroundColor
+    element.style.backgroundColor = `${currentColor}`
+}
+input.addEventListener('change', (e) => changeCurrentColor(e)) //Quando o evento 'change' é acionado no input a função changeCurrentColor(e) será chamada.
+
+createTable();
